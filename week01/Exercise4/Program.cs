c813df0fab2b36524fa88b54801f00582e53ed5b
t_developer <towolabi@byupathway.edu>
2025-11-03 19:43:00 +0100

    Finished with exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int listNumber;
+
+        do
+        {
+            Console.Write("Enter a number here and type 0 to stop the game:  ");
+            listNumber = int.Parse(Console.ReadLine());
+
+            if (listNumber != 0)
+            {
+                numbers.Add(listNumber);
+            }
+
+        } while (listNumber != 0); // condition checked after the loop runs once
+
+        // Calculate sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Calculate average
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Find max
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
